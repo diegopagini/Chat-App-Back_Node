ce822d75ce8a7730f1093dca685eab9380d23cda
Diego Pagini <diego.pagini@neoris.com>
2023-03-25 17:58:28 +0100

    create a user in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,36 @@
 /** @format */
 
 const { response, request } = require('express');
+const Usuario = require('../models/user');
 
 const createUser = async (req = request, res = response) => {
-	res.json({
-		ok: true,
-		msg: 'create user',
-	});
+	try {
+		const { email, password } = req.body;
+		// Check if email already used:
+		const emailExists = await Usuario.findOne({ email });
+		if (emailExists)
+			return res.status(400).json({
+				ok: false,
+				msg: 'Email already used',
+			});
+
+		// Encrypt password:
+
+		// Save user in DB:
+		const user = new Usuario(req.body);
+		await user.save();
+
+		return res.status(200).json({
+			ok: true,
+			user,
+		});
+	} catch (error) {
+		console.log(error);
+		return res.status(500).json({
+			ok: false,
+			msg: 'Talk to the administrator',
+		});
+	}
 };
 
 const login = async (req = request, res = response) => {
