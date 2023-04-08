51c917391ecb5d0ed86efb1bcdd150d4aaf672af
Diego Pagini <diegopaginidev@gmail.com>
2023-04-08 10:02:50 +0200

    socket client

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -25,6 +25,16 @@ const generateJWT = (uid) => {
 	});
 };
 
+const checkJWT = (token = '') => {
+	try {
+		const { uid } = jwt.verify(token, process.env.JWT_KEY);
+		return [true, uid];
+	} catch (error) {
+		return [false, null];
+	}
+};
+
 module.exports = {
+	checkJWT,
 	generateJWT,
 };
--- models/sockets.js
+++ models/sockets.js
@@ -1,5 +1,7 @@
 /** @format */
 
+const { checkJWT } = require('../helpers/jwt');
+
 class Sockets {
 	constructor(io) {
 		this.io = io;
@@ -11,6 +13,19 @@ class Sockets {
 		// On connection:
 		this.io.on('connection', (socket) => {
 			/** socket connections */
+
+			const [valid, uid] = checkJWT(socket.handshake.query['x-token']);
+
+			if (!valid) {
+				console.log('Unidentified socket');
+				return socket.disconnect();
+			}
+
+			console.log('Client connected', uid);
+
+			socket.on('disconnect', () => {
+				console.log('Disconnected client', uid);
+			});
 		});
 	}
 }
